fdbedf9fe7312b703016806075c95228f999bf08
Ashe3 <Korolewsky.anton3@gmail.com>
2019-11-01 21:08:45 +0200

    add travis/eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const greetings = () => {
-    const name = readlineSync.question("May I have your name? ");
+const greetings = () => {
+    const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
+};
+
+export default {
+    greetings,
 };
