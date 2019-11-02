64cf0812d944ff5ddc502ed41fd8cea98adae7b1
Ashe3 <Korolewsky.anton3@gmail.com>
2019-11-02 20:51:08 +0200

    brain-even minor changes

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -7,30 +7,23 @@ console.log('Answer "yes" if the number is even, otherwise asnwer "no".\n');
 const name = greetings();
 const ERR_VAL = 1;
 
+const wrongAnswer = (answer, userName) => {
+    console.log(`'${answer}' is wrong asnwer ;(. Correct answer was '${answer === 'yes' ? 'no' : 'yes'}'.`);
+    console.log(`Let's try again, ${userName}!\n`);
+    return ERR_VAL;
+};
+
 const game = (userName) => {
-    const wrongAnswer = (answer) => {
-        console.log(`'${answer}' is wrong asnwer ;(. Correct answer was '${answer === 'yes' ? 'no' : 'yes'}'.`);
-        console.log(`Let's try again, ${userName}!\n`);
-        return ERR_VAL;
-    };
     for (let i = 0; i < 3; i += 1) {
         const number = Math.round(Math.random() * 100);
         console.log(`Question: ${number}`);
         const answer = readlineSync.question('Your answer: ');
-        if (number % 2 === 0) {
-            if (answer === 'yes') {
-                console.log('Correct!');
-            } else {
-                return wrongAnswer(answer);
-            }
-        } else if (number % 2 !== 0) {
-            if (answer === 'no') {
-                console.log('Correct!');
-            } else {
-                return wrongAnswer(answer);
-            }
+        if (number % 2 === 0 && answer === 'yes') {
+            console.log('Correct!');
+        } else if (number % 2 !== 0 && answer === 'no') {
+            console.log('Correct!');
         } else {
-            return wrongAnswer(answer);
+            return wrongAnswer(answer, userName);
         }
     }
     console.log(`Congratulations, ${userName}!\n`);
