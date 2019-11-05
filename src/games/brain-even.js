a0ac358080cd481ebffc8179107dab3c74694a28
Ashe3 <Korolewsky.anton3@gmail.com>
2019-11-05 22:15:24 +0200

    small nf fixes

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,13 +1,13 @@
 import * as games from '../index';
 
-const maxGenValue = 250;
-const minGenValue = 1;
+const maxGeneratedValue = 250;
+const minGeneratedValue = 1;
 const gamerule = 'Answer "yes" if the number is even, otherwise asnwer "no".\n';
 
 const calculateAnswer = (number) => (games.isEven(number) ? 'yes' : 'no');
 
 const generateGameData = () => {
-    const gameCondition = games.generateRandNumber(maxGenValue, minGenValue);
+    const gameCondition = games.generateRandNumber(maxGeneratedValue, minGeneratedValue);
     const correctAnswer = calculateAnswer(gameCondition);
 
     return {
