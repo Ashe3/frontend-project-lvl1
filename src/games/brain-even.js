feb43e56f36275cc790709db1ca7427024ccdd95
Ashe3 <Korolewsky.anton3@gmail.com>
2019-11-24 14:58:58 +0200

    small refactores/complete readme

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,11 +5,11 @@ const maxGeneratedValue = 250;
 const minGeneratedValue = 1;
 const gameRule = 'Answer "yes" if the number is even, otherwise asnwer "no".';
 
-const isEven = (number) => number % 2 === 0;
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
 const generateGameData = () => {
     const gameCondition = generateRandNumber(minGeneratedValue, maxGeneratedValue);
-    const correctAnswer = isEven(gameCondition) ? 'yes' : 'no';
+    const correctAnswer = isEven(gameCondition);
 
     return {
         gameCondition,
