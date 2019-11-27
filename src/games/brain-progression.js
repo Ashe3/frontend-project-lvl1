a8b5d6e1a2d8b1111de188d3bf7dae778d28e412
Пользователь <User@COMP-VID.koleso.local>
2019-11-27 10:16:39 +0200

    'ref'

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,18 +4,18 @@ import generateRandNumber from '../utils';
 const progressionCount = 10;
 const maxDifferenceValue = 9;
 const maxGenValue = 100;
-const minGenValue = 1;
+const minGenValue = 0;
 const gameRule = 'What number is missing in the progression?';
 
 const generateGameData = () => {
-    const lostValue = generateRandNumber(progressionCount - 1, minGenValue);
-    const difference = generateRandNumber(minGenValue, maxDifferenceValue);
+    const lostValueIndex = generateRandNumber(progressionCount - 1, minGenValue);
+    const difference = generateRandNumber(minGenValue + 1, maxDifferenceValue);
     let currerntValue = generateRandNumber(minGenValue, maxGenValue);
-    const correctAnswer = String(currerntValue + difference * lostValue);
+    const correctAnswer = String(currerntValue + difference * lostValueIndex);
     let gameCondition = '';
 
     for (let i = 0; i < progressionCount; i += 1) {
-        if (i === lostValue) {
+        if (i === lostValueIndex) {
             gameCondition = `${gameCondition} ..`;
         } else {
             gameCondition = `${gameCondition} ${currerntValue}`;
